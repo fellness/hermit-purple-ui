a7482fbfe934e38e0d4ebb92705a3ca0092b862f
homura <homura.dev@gmail.com>
2020-02-18 14:12:07 +0800

    feat: make timestamps more humanized

--- src/container/Timestamp.tsx
+++ src/container/Timestamp.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import { Tooltip } from 'antd';
+
+dayjs.extend(relativeTime);
+dayjs.extend(localizedFormat);
 
 interface TimestampProps {
   timestamp: dayjs.ConfigType;
 }
 
 export function Timestamp(props: TimestampProps) {
-  return <>{dayjs(props.timestamp).format('YYYY-MM-DD HH:mm:ss')}</>;
+  const time = dayjs(props.timestamp);
+  return <Tooltip title={time.format('L LT')}>{time.toNow()}</Tooltip>;
 }
