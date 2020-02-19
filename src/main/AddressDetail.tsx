370f7933c7464966c0b411d88405cbb3055d0671
homura <homura.dev@gmail.com>
2020-02-19 09:36:55 +0800

    feat: supported balance list in address detail

--- src/main/AddressDetail.tsx
+++ src/main/AddressDetail.tsx
@@ -1,20 +1,77 @@
 import React from 'react';
-import { PageHeader, Table } from 'antd';
-import { useParams } from 'react-router-dom';
+import { Divider, Empty } from 'antd';
+import { Link, useParams } from 'react-router-dom';
 import { SimplePagination } from '../container/SimplePage';
 import { useTranslation } from 'react-i18next';
 import gql from 'graphql-tag';
 import { usePageQuery } from '../hook/use-page-query';
 import {
+  AccountBalancesQuery,
+  AccountBalancesQueryVariables,
   AccountTransactionQuery,
   AccountTransactionQueryVariables,
 } from '../generated/types';
 import { $ElementType, ValuesType } from 'utility-types';
 import { ColumnProps } from 'antd/es/table';
-import { Link } from 'react-router-dom';
 import { BlockHeight } from '../container/BlockHeight';
 import { hexToNum } from '../utils';
 import _ from 'lodash';
+import { StyledTable } from '../styled/Table';
+import { useQuery } from '@apollo/react-hooks';
+import { StyledCard } from '../styled/Card';
+
+const QUERY_ACCOUNT_BALANCE = gql`
+  query accountBalances($address: String!) {
+    account(where: { address: $address }) {
+      balances {
+        asset {
+          assetId
+          name
+          symbol
+        }
+        balance
+      }
+    }
+  }
+`;
+
+function AccountBalanceList(props: { address: string }) {
+  const { t } = useTranslation();
+  const { data } = useQuery<
+    AccountBalancesQuery,
+    AccountBalancesQueryVariables
+  >(QUERY_ACCOUNT_BALANCE, { variables: { address: props.address } });
+
+  const account = data?.account;
+  if (!account) return null;
+
+  const columns: ColumnProps<ValuesType<typeof account.balances>>[] = [
+    {
+      title: t('Name'),
+      dataIndex: 'asset.name',
+    },
+
+    {
+      title: t('Symbol'),
+      dataIndex: 'asset.symbol',
+    },
+    {
+      title: t('Balance'),
+      dataIndex: 'balance',
+      render: hexToNum,
+    },
+  ];
+
+  return (
+    <StyledTable
+      title={() => t('Holding')}
+      pagination={false}
+      size="small"
+      dataSource={account.balances}
+      columns={columns}
+    />
+  );
+}
 
 const QUERY_ACCOUNT_TRANSACTION = gql`
   query accountTransaction(
@@ -96,14 +153,22 @@ export function AddressDetail() {
     },
   ];
 
+  if (!address) return <Empty />;
+
   // TODO: query order by desc
   const transactions = _.reverse(data?.account?.transactions ?? []);
   const length = transactions?.length;
   const hasNextPage = length === undefined ? false : length >= 10;
 
   return (
-    <PageHeader title={t('Address')} subTitle={address}>
-      <Table
+    <StyledCard title={t('Address')}>
+      <AccountBalanceList address={address} />
+
+      <Divider />
+
+      <StyledTable
+        size="small"
+        title={() => t('Transactions')}
         loading={loading}
         columns={columns}
         pagination={false}
@@ -115,6 +180,6 @@ export function AddressDetail() {
         hasNextPage={hasNextPage}
         hasPrevPage={page > 1}
       />
-    </PageHeader>
+    </StyledCard>
   );
 }
